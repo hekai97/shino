e7d43eebc1eb180be662ef827da81158a1421cc2
whale789 <1974396570@qq.com>
2022-09-12 10:49:18 +0800

    统计分析和深度分析

---
+++ FrontEnd/admin/src/views/test.vue
@@ -0,0 +1,35 @@
+<!--<template>-->
+
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "test",-->
+<!--  methods:{-->
+<!--    myfunction(){-->
+<!--      const myObject=new URLSearchParams();-->
+<!--      const myObject2=new FormData();-->
+<!--      myObject.set("page","123");-->
+<!--      myObject.set("size","123");-->
+<!--      axios({-->
+<!--        uri:xxxx,-->
+<!--        data:myObject-->
+<!--      })-->
+<!--    },-->
+<!--    myfunction2(){-->
+<!--      const myObject={-->
+<!--        id:12,-->
+<!--        name:wxxx -->
+<!--      }-->
+<!--      axios({-->
+<!--        uri:xxxx,-->
+<!--        data:myObject-->
+<!--      })-->
+<!--    }-->
+
+<!--  }-->
+<!--}-->
+<!--</script>-->
+<!--<style scoped>-->
+
+<!--</style>-->
