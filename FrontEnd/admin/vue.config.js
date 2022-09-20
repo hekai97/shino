e352530f7041fbf65de8615772cc5ef8b63d5c3c
201958503142 <2769108097@qq.com>
2022-09-20 11:12:24 +0800

    角色管理页面

--- FrontEnd/admin/vue.config.js
+++ FrontEnd/admin/vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    resolve: {
+      fallback: { path: require.resolve("path-browserify") },
+    },
+  },
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
